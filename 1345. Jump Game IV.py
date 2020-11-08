4246aa44bdca9ec70aa1349e250a4c64c89d84fe
cszhao <787662335@qq.com>
2020-11-08 15:27:39 +0800

    add annotation to 1345

--- 1345. Jump Game IV.py
+++ 1345. Jump Game IV.py
@@ -4,17 +4,17 @@ class Solution:
     def minJumps(self, arr: List[int]) -> int:
         m = {}
         for p, val in enumerate(arr):
-            if 0 < p < len(arr)-1 and val == arr[p-1] == arr[p+1]:
+            if 0 < p < len(arr)-1 and val == arr[p-1] == arr[p+1]: #连续的数字只记录两端的index
                 continue
             if val not in m.keys():
                 m[val] = [p]
             else:
                 m[val].append(p)
         # print(m)
-        q = [(0, 0)]
-        flag = set([0])
+        q = [(0, 0)] #BFS，从0开始跳，此时step=0
+        flag = set([0]) #已访问过的index，不会访问第二次
         step = 0
-        while q:
+        while q: #BFS开始
             p, step = q.pop(0)
             # print(p)
             if p == len(arr)-1:
@@ -25,11 +25,11 @@ class Solution:
             # if p < len(arr)-1 and p+1 not in flag:
             #     flag.add(p+1)
             #     q.append((p+1, step+1))
-            for pp in [p-1, p+1] + m[arr[p]]:
-                if pp not in flag:
-                    if 0 <= pp < len(arr):
-                        flag.add(pp)
-                        q.append((pp, step+1))
+            for pp in [p-1, p+1] + m[arr[p]]: #当前BFS可选的index
+                if pp not in flag: #还没有达到过的index
+                    if 0 <= pp < len(arr): #index合法
+                        flag.add(pp) #记录index
+                        q.append((pp, step+1)) #jump
         return step
 
 arr = [100,-23,-23,404,100,23,23,23,3,404]
